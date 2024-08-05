657b20b854d0645d4c45763d6cae43f4da0eb3fd
Viktor Maly <vicnyr@gmail.com>
2024-08-05 18:00:24 +0100

    I created a grid and added an event that allows to change color

--- project/main.js
+++ project/main.js
@@ -1,17 +1,47 @@
 let gridContainer = document.querySelector("#grid-container");
+//new class
+//gridContainer.classList.add("painting")
+
 let child = document.createElement("div");
-child.classList.add("child");
-
-child.innerHTML = "Cube"
-
-gridContainer.appendChild(child)
 
 
-let quadrado = "50px"
-let valor = 800 / quadrado
-let merda = valor * valor
-child.style.height = quadrado;
-child.style.width = quadrado;
+
+child.style.backgroundColor = "red";
+
+
+function calculateGridWidth(line){
+  let gridNumber = line * line
+  let width1 = 800 / line;
+  let squareLength = width1 + "px"  
+
+  for(i = 1; i <= gridNumber; i++){
+  let child = document.createElement("div");
+  child.classList.add("child");
+   let side = gridContainer.appendChild(child);
+   side.style.height = squareLength;
+   side.style.width = squareLength;
+  }
+}
+
+
+gridContainer.onmouseover = function(event){
+  let target = event.target;
+  target.style.backgroundColor = "pink";
+}
+/*
+gridContainer.onmouseout = function(event) {
+  let target = event.target;
+  target.style.background = '';
+}*/
+
+calculateGridWidth(16)
+
+
+
+
+
+
+
 
 
 
